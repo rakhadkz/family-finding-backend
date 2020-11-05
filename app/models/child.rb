6b1c8b9369a176a8802488bcb75a8ea985fa9346
Shyngys Rakhad <dev.chinga@gmail.com>
2020-11-05 20:17:27 +0600

    add: children to contacts relationship

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,4 @@
 class Child < ApplicationRecord
   has_many :attachments
+  has_and_belongs_to_many :contacts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_195719) do
+ActiveRecord::Schema.define(version: 2020_11_05_140716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "contacts", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "birthday"
-    t.string "address"
-    t.string "address_2"
-    t.string "city"
-    t.string "state"
-    t.string "email"
-    t.string "phone"
-    t.string "zip"
-
-    create_table "attachments", force: :cascade do |t|
+  create_table "attachments", force: :cascade do |t|
     t.bigint "child_id"
     t.string "filename"
     t.string "filetype"
@@ -53,6 +41,28 @@ ActiveRecord::Schema.define(version: 2020_11_04_195719) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "birthday"
+    t.string "address"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "email"
+    t.string "phone"
+    t.string "zip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts_children", id: false, force: :cascade do |t|
+    t.bigint "contact_id"
+    t.bigint "child_id"
+    t.index ["child_id"], name: "index_contacts_children_on_child_id"
+    t.index ["contact_id"], name: "index_contacts_children_on_contact_id"
   end
 
   create_table "organizations", force: :cascade do |t|
