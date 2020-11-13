7c2e1ff72c24957665b3e9841ec47135d49b1301
Murat Tishkul <tishkul@unist.ac.kr>
2020-11-14 01:26:41 +0600

    fixed forgot password functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,9 +4,9 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = 'Lorem Ipsum <info@lorem-ipsum.io>'
+  config.mailer_sender = 'andrewfamilyfinding@weblightdevelopment.com'
 
-  # Configure the class responsible to send e-mails.
+  # Configure the class responsible to send e-mails. andrewfamilyfinding@weblightdevelopment.com
   # config.mailer = "Devise::Mailer"
 
   # ==> ORM configuration
