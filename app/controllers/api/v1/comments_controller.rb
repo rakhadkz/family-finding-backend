039d296d987d7482f58514027d6465d66af3546f
Sanjar Giyaziddinov <giyaziddinov@gmail.com>
2020-11-14 01:02:37 +0500

    Refactor Comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,14 +1,8 @@
 class Api::V1::CommentsController < ApplicationController
-  include Filterable
-  include Searchable
-  include Sortable
-
   before_action :authenticate_request!
-  before_action :set_comment, only: [:show]
-  before_action :my_comment, only: [:update, :destroy]
 
   def show
-    render json: CommentBlueprint.render(comments, root: :data)
+    render json: CommentBlueprint.render(comment, root: :data)
   end
 
   def create
@@ -17,27 +11,19 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   def update
-    @my_comment.update!(comment_params)
-    render json: SearchVectorBlueprint.render(@my_comment, root: :data)
+    comment.update!(comment_params)
+    render json: SearchVectorBlueprint.render(comment, root: :data)
   end
 
   def destroy
-    @my_comment.destroy
-    render status: :ok
+    comment.destroy!
+    head :ok
   end
 
   private
 
-  def comment_scope
-    Comment.all
-  end
-
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  def my_comment
-    @my_comment = @current_user.comments.find(params[:id])
+  def comment
+    @comment ||= Comment.find(params[:id])
   end
 
   def comment_params
@@ -48,5 +34,4 @@ class Api::V1::CommentsController < ApplicationController
                     :in_reply_to
                 ])
   end
-
 end
