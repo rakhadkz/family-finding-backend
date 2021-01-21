8ef8eee5a9076d2a9f1b9cad5f295060e573d224
Murat Tishkul <tishkul@unist.ac.kr>
2021-01-21 21:20:18 +0600

    added org id to search vectors, fixed controllers and models

--- app/models/search_vector.rb
+++ app/models/search_vector.rb
@@ -1,4 +1,16 @@
 class SearchVector < ApplicationRecord
   has_many :findings
   has_many :children, through: :findings
+  belongs_to :organization
+
+  include PgSearch::Model
+
+  pg_search_scope :search,
+    against: [
+      :name, :description
+    ],
+    using: {
+      tsearch: {prefix: true}
+    }
+
 end
