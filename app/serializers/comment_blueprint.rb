a84d5c9c1cabcf4600f06e7ee6bfc7eb96eeb754
Shyngys Rakhad <dev.chinga@gmail.com>
2021-02-12 02:37:54 +0600

    fix: blueprints

--- app/serializers/comment_blueprint.rb
+++ app/serializers/comment_blueprint.rb
@@ -2,6 +2,10 @@ class CommentBlueprint < Blueprinter::Base
   identifier :id
   fields :user_id, :title, :body, :html_body, :created_at, :updated_at, :child_id, :mentions
 
+  view :user do
+    association :user, blueprint: UserBlueprint
+  end
+
   view :extended do
     association :parent, blueprint:  CommentBlueprint, name: :in_reply_to
     association :replies, blueprint:  CommentBlueprint, view: :extended
--- app/serializers/contact_blueprint.rb
+++ app/serializers/contact_blueprint.rb
@@ -1,10 +1,6 @@
 class ContactBlueprint < Blueprinter::Base
   identifier :id
   fields :first_name, :last_name,:relationship, :birthday, :address, :address_2, :city, :state, :zip, :email, :phone
-<<<<<<< HEAD
-=======
 
-
->>>>>>> d71d0c7d566aedd72edd442ce95cb479f837d447
 end
   
