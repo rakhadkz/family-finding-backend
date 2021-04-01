c1ee5e528f20f187ece2aca97d892e63090aa9dd
Shyngys Rakhad <dev.chinga@gmail.com>
2021-04-01 19:31:20 +0600

    feat/make call_webhook universal

--- app/controllers/api/v1/search_jobs_controller.rb
+++ app/controllers/api/v1/search_jobs_controller.rb
@@ -17,17 +17,30 @@ class Api::V1::SearchJobsController < ApplicationController
     dataset_id = search_job_params[:webhook]["default_dataset_id"]
     raise ArgumentError.new "No dataset provided" if dataset_id.nil?
     response = URI.parse("https://api.apify.com/v2/datasets/#{dataset_id}/items?clean=true&format=json&token=#{ENV["APIFY_TOKEN"]}").read
-    description = JSON.parse(response)[0]["description"]
     family_search_id = JSON.parse(response)[0]["family_search_id"]
     raise ArgumentError.new "Family Search not found" if family_search_id.nil?
     family_search = FamilySearch.find(family_search_id)
+    description = JSON.parse(response)[0]["description"]
     if Digest::SHA1.hexdigest(description) != family_search.hashed_description
-      family_search.description = description
-      family_search.hashed_description = Digest::SHA1.hexdigest(description)
-      family_search.is_link_alert = true if family_search.is_link_alert === false || family_search.is_link_alert.nil?
+      is_link_alert = JSON.parse(response)[0]["is_link_alert"] || false
+      family_search.update!(
+        description: description,
+        hashed_description: Digest::SHA1.hexdigest(description),
+        is_link_alert: is_link_alert,
+        date_completed: Time.now
+      )
+      attachments = JSON.parse(response)[0]["attachments"] || []
+      attachments.each do |attachment|
+        next if Attachment.where(file_name: attachment["file_name"]).count > 0
+        new_attachment = Attachment.create!(**attachment, user_id: family_search.user_id)
+        new_attachment.child_contact_attachments.create!(child_contact_id: family_search.child_contact_id)
+      end
+      contacts = JSON.parse(response)[0]["contacts"] || []
+      contacts.each do |contact|
+        new_contact = Contact.create!(contact.except("relationship"))
+        new_contact.child_contacts.create!(child_id: family_search.child_id, relationship: contact["relationship"])
+      end
     end
-    family_search.date_completed = Time.now
-    family_search.save
     render status: :ok
   end
 
@@ -40,8 +53,7 @@ class Api::V1::SearchJobsController < ApplicationController
   end
 
   def call_apify_task
-    if search_job_params[:family_search_id].present?
-      family_search = FamilySearch.find search_job_params[:family_search_id]
+    if family_search
       raise ArgumentError.new "Connection id cannot be nil" if family_search.child_contact_id.nil?
       send_request family_search
     else
@@ -74,7 +86,7 @@ class Api::V1::SearchJobsController < ApplicationController
   end
 
   def send_request(family_search)
-    return if family_search.child_contact.nil?
+    raise ArgumentError.new "Connection of the Search Result cannot be null" if family_search.child_contact.nil?
     family_search.update date_completed: nil
     uri = URI.parse("https://api.apify.com/v2/actor-tasks/#{search_vector.task_id}/runs?token=#{ENV["APIFY_TOKEN"]}")
     header = { "Content-Type": 'application/json' }
@@ -84,21 +96,7 @@ class Api::V1::SearchJobsController < ApplicationController
     http.read_timeout = 100
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = body.to_json
-    response = JSON.parse(http.request(request).body)
-
-    puts "======================"
-    puts response
-    puts "======================"
-
-
-    # if Digest::SHA1.hexdigest(response).to_s != family_search.hashed_description
-    #   family_search.description = response
-    #   family_search.hashed_description = Digest::SHA1.hexdigest(response).to_s
-    #   family_search.is_link_alert = true if family_search.is_link_alert === false || family_search.is_link_alert.nil?
-    # end
-
-    # family_search.date_completed = Time.now
-    # family_search.save
+    JSON.parse(http.request(request).body)
   end
 
   def request_body(family_search)
@@ -109,7 +107,7 @@ class Api::V1::SearchJobsController < ApplicationController
       customData: {
         firstName: first_name,
         lastName: last_name,
-
+        birthday: birthday.present? ? birthday.strftime("%m%d%Y") : nil,
         family_search_id: family_search.id
       }
     }
