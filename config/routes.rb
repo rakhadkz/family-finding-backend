5f6e36a934dcb18d18d9bfc8cf3f7525af96b315
Murat Tishkul <murat.tishkul@gmail.com>
2021-05-13 23:57:07 +0600

    created reports controller for reports page

--- config/routes.rb
+++ config/routes.rb
@@ -95,6 +95,11 @@ Rails.application.routes.draw do
             post 'send_request'
           end
         end
+        namespace :reports do
+          match :children, via: [:get, :post]
+          match :placements, via: [:get, :post]
+          match :linked_connections, via: [:get, :post]
+        end
       end
 
       devise_for :users
